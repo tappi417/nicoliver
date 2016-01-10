4cd9f8afe36542f6fc27e94a269be8c1a6736421
meteorakey <meteorakey@gmail.com>
2016-01-10 12:47:48 +0900

    生放送リンク取得，機能分解（クラス化）

--- core.rb
+++ core.rb
@@ -3,12 +3,51 @@ require 'uri'
 require 'open-uri'
 require 'nokogiri'
 
+# チャンネルページのデータクラス
+class ChannelContent
+  attr_accessor :live_contents
+  def initialize
+    @live_contents = Array.new
+  end
+end
 
-# チャンネルページデータを取得　
-url = URI.parse('http://ch.nicovideo.jp/amiami-ch')
-doc = Nokogiri::HTML(open(url))
+# 生放送のデータクラス
+class LiveContent
+  attr_accessor :title, :live_url, :broadcast_date
+  @title
+  @live_url
+  @broadcast_date
+end
 
-doc.css('.p-live-body').each do |title|
-  puts title.content.gsub(/(\s)+/, '')
+
+# チャンネルページデータを取得
+def aquire_content(pageurl)
+  url = URI.parse(pageurl)
+  doc = Nokogiri::HTML(open(url))
+
+  channel_content = ChannelContent.new
+  doc.css('.p-live-body').each do |live|
+    # 生放送データ取得(タイトル，リンク取得でループするが1つのみ取得できる想定)
+    live.css('.g-live-title').each do |title|
+      live_content = LiveContent.new
+      live_content.title = title.content.gsub(/(\s)+/, '')
+      title.css('a').each do |anchor|
+        live_content.live_url = anchor['href']
+      end
+      channel_content.live_contents << live_content
+    end
+  end
+  return channel_content
+end
+
+# 各チャンネルページのデータを取得
+urls = Array['http://ch.nicovideo.jp/amiami-ch',
+             'http://ch.nicovideo.jp/imas-station']
+
+urls.each do |url|
+  channel_content = aquire_content(url)
+  channel_content.live_contents.each do |live|
+    puts live.title + ": " + live.live_url
+  end
 end
 
