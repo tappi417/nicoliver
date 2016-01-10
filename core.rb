00e66277b37f41454891b8ac7e4317ffcaa57cdb
meteorakey <meteorakey@gmail.com>
2016-01-10 15:38:15 +0900

    日付取得見直し（年の取得ロジック追加）

--- core.rb
+++ core.rb
@@ -2,11 +2,13 @@
 require 'uri'
 require 'open-uri'
 require 'nokogiri'
+require 'date'
 
 # チャンネルページのデータクラス
 class ChannelContent
-  attr_accessor :live_contents
-  def initialize
+  attr_accessor :channel_url, :live_contents
+  def initialize(channel_url)
+    @channel_url = channel_url
     @live_contents = Array.new
   end
 end
@@ -17,14 +19,32 @@ class LiveContent
   def initialize
     @title = ""
     @live_url = ""
+    # Datetimeで扱う
     @broadcast_date = ""
   end
 end
 
 # 放送日の生データを加工
 def format_date(raw_date)
-  result = raw_date.gsub(/(\s)+|放送予定|放送終了|/, '')
-  return result[0, 5] + " " + result[5, 13]
+  date = raw_date.gsub(/(\s)+|放送予定|放送終了|/, '')
+  now_date = DateTime.now
+
+  month = date[0, 2].to_i
+  day = date[3, 2].to_i
+  hour = date[5, 2].to_i
+  minutes = date[8, 2].to_i
+
+  # 現在が12月で取得した月が1月の場合
+  if (now_date.month == 12 && month == 1)
+    year = now_date.year + 1
+  # 現在が1月で取得した月が12月の場合
+  elsif (now_date.month == 1 && month == 12)
+    year = now_date.year - 1
+  else
+    year = now_date.year
+  end
+
+  return DateTime.new(year, month, day, hour, minutes, 00)
 end
 
 # チャンネルページデータを取得
@@ -32,7 +52,7 @@ def aquire_content(pageurl)
   url = URI.parse(pageurl)
   doc = Nokogiri::HTML(open(url))
 
-  channel_content = ChannelContent.new
+  channel_content = ChannelContent.new(pageurl)
   doc.css('.p-live-body').each do |live|
     live_content = LiveContent.new
     # タイトル，リンク取得（ループするが1つのみ取得できる想定)
@@ -53,12 +73,18 @@ end
 
 # 各チャンネルページのデータを取得
 urls = Array['http://ch.nicovideo.jp/amiami-ch',
-             'http://ch.nicovideo.jp/imas-station']
+             'http://ch.nicovideo.jp/imas-station',
+             'http://ch.nicovideo.jp/MillionRADIO',
+             'http://ch.nicovideo.jp/cinderellaparty',
+             'http://ch.nicovideo.jp/lulucan-himitsu']
 
 urls.each do |url|
   channel_content = aquire_content(url)
   channel_content.live_contents.each do |live|
-    puts live.title + ": " + live.live_url + "(" + live.broadcast_date + ")"
+    # 放送日を迎えていない生放送のみ表示
+    if (live.broadcast_date > DateTime.now)
+      puts live.title + ": " + live.live_url + "(" + live.broadcast_date.strftime("%Y/%m/%d %a.") + ")"
+    end
   end
 end
 
