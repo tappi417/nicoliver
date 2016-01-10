31b2c7fb681ef25957a7056bf6c733296dcf3f3b
meteorakey <meteorakey@gmail.com>
2016-01-10 13:44:57 +0900

    放送日取得

--- core.rb
+++ core.rb
@@ -14,11 +14,18 @@ end
 # 生放送のデータクラス
 class LiveContent
   attr_accessor :title, :live_url, :broadcast_date
-  @title
-  @live_url
-  @broadcast_date
+  def initialize
+    @title = ""
+    @live_url = ""
+    @broadcast_date = ""
+  end
 end
 
+# 放送日の生データを加工
+def format_date(raw_date)
+  result = raw_date.gsub(/(\s)+|放送予定|放送終了|/, '')
+  return result[0, 5] + " " + result[5, 13]
+end
 
 # チャンネルページデータを取得
 def aquire_content(pageurl)
@@ -27,14 +34,18 @@ def aquire_content(pageurl)
 
   channel_content = ChannelContent.new
   doc.css('.p-live-body').each do |live|
-    # 生放送データ取得(タイトル，リンク取得でループするが1つのみ取得できる想定)
+    live_content = LiveContent.new
+    # タイトル，リンク取得（ループするが1つのみ取得できる想定)
     live.css('.g-live-title').each do |title|
-      live_content = LiveContent.new
       live_content.title = title.content.gsub(/(\s)+/, '')
       title.css('a').each do |anchor|
         live_content.live_url = anchor['href']
       end
       channel_content.live_contents << live_content
+    end
+    # 放送日取得
+    live.css('.g-live-airtime').each do |date|
+      live_content.broadcast_date = format_date(date.content)
     end
   end
   return channel_content
@@ -47,7 +58,7 @@ urls = Array['http://ch.nicovideo.jp/amiami-ch',
 urls.each do |url|
   channel_content = aquire_content(url)
   channel_content.live_contents.each do |live|
-    puts live.title + ": " + live.live_url
+    puts live.title + ": " + live.live_url + "(" + live.broadcast_date + ")"
   end
 end
 
