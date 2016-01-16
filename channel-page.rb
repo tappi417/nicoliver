9df2e80c5bc6f4842528882635dbf4abce6d4dfe
meteorakey <meteorakey@gmail.com>
2016-01-16 13:23:03 +0900

    チャンネルの登録/削除機能追加（DBへの登録/削除のみ）

--- channel-page.rb
+++ channel-page.rb
@@ -1,20 +1,46 @@
 # -*- coding: utf-8 -*-
+require 'sqlite3'
+
+# データベース操作クラス
+class SQLExcecutor
+
+  def initialize()
+    @db = SQLite3::Database.new("data.db")
+  end
+
+  #insert
+  def insert_channel(channel)
+    @db.execute("insert into channel values(?, ?)",  channel.channel_url, channel.channel_title)
+  end
+
+  #delete
+  def delete_channel(channel)
+    @db.execute("delete from channel where channel_url = ?", channel.channel_url)
+  end
+
+end
+
 # チャンネルページのデータクラス
-class ChannelContent
-  attr_accessor :channel_url, :live_contents
+class Channel
+  attr_accessor :channel_url, :channel_title, :lives
   def initialize(channel_url)
     @channel_url = channel_url
-    @live_contents = Array.new
+    @channel_title = ""
+    @lives = Array.new
   end
+
+
+  #select
+
 end
 
 # 生放送のデータクラス
-class LiveContent
-  attr_accessor :title, :live_url, :broadcast_date
+class Live
+  attr_accessor :channel_url, :live_url, :live_title, :broadcast_date
   def initialize
-    @title = ""
+    @channel_url = ""
+    @live_title = ""
     @live_url = ""
-    # Datetimeで扱う
-    @broadcast_date = ""
+    @broadcast_date = "" # Datetimeで扱う
   end
 end
